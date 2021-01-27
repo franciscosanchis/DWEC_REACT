6a6414f251f8701ba9355867cd7fa2bad1de4311
franciscosanchis <56738943+franciscosanchis@users.noreply.github.com>
2021-01-27 18:22:52 +0100

    Act10.3_FSH: Casi finalizada Falta revisar la rubrica y comprobar que está todo lo requerido

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,51 @@ import ImageList from "./ImageList";
 import Paginator from "./Paginator";
 
 class App extends React.Component {
-  state = { images: [], pagAtras: 0 };
+  state = {
+    images: [],
+    page: 1,
+    currentTerm: "",
+    totalPages: 0
+  };
 
-  onSearchSubmit = async (term, pag) => {
+  onSearchSubmit = async (term, page) => {
+    if (this.state.currentTerm !== term) {
+      this.setState({ currentTerm: term, page: 1 });
+    }
     //https://unsplash.com/documentation#search-photos
     const resp = await unsplash.get("/search/photos", {
-      params: { query: term }
+      params: { query: term, page }
     });
 
-    this.setState({ images: resp.data.results });
+    this.setState({
+      images: resp.data.results,
+      totalPages: resp.data.total_pages
+    });
+  };
+
+  isEmpty() {
+    if (this.state.images.length !== 0) return false;
+    else return true;
+  }
+  isFirstPage() {
+    if (this.state.page === 1) return true;
+  }
+  isLastPage() {
+    if (this.state.page === this.state.totalPages) return true;
+  }
+
+  getcallbackValueNext = () => {
+    let newpage = this.state.page + 1;
+    let actualterm = this.state.currentTerm;
+    this.setState({ page: this.state.page + 1, term: this.state.currentTerm });
+    this.onSearchSubmit(actualterm, newpage);
+  };
+
+  getcallbackValueBack = () => {
+    let newpage = this.state.page - 1;
+    let actualterm = this.state.currentTerm;
+    this.setState({ page: this.state.page - 1, term: this.state.currentTerm });
+    this.onSearchSubmit(actualterm, newpage);
   };
 
   render() {
@@ -21,7 +57,13 @@ class App extends React.Component {
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
         <ImageList images={this.state.images} />
-        <Paginator />
+        <Paginator
+          isEmpty={this.isEmpty()}
+          isFirstPage={this.isFirstPage()}
+          isLastPage={this.isLastPage()}
+          callbackValueNext={() => this.getcallbackValueNext()}
+          callbackValueBack={() => this.getcallbackValueBack()}
+        />
       </div>
     );
   }
--- src/components/Paginator.js
+++ src/components/Paginator.js
@@ -1,18 +1,30 @@
 import React from "react";
 
-const Paginator = (props) => {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <button class="ui labeled icon button">
-        <i class="left arrow icon"></i>
-        Atras
-      </button>
-      <button class="ui right labeled icon button">
-        <i class="right arrow icon"></i>
-        Adelante
-      </button>
-    </div>
-  );
-};
-
+class Paginator extends React.Component {
+  render() {
+    if (this.props.isEmpty) return "";
+    else {
+      return (
+        <div style={{ textAlign: "center" }}>
+          <button
+            disabled={this.props.isFirstPage}
+            className="ui labeled icon button"
+            onClick={() => this.props.callbackValueBack()}
+          >
+            <i className="left arrow icon"></i>
+            Atras
+          </button>
+          <button
+            disabled={this.props.isLastPage}
+            className="ui right labeled icon button"
+            onClick={() => this.props.callbackValueNext()}
+          >
+            <i className="right arrow icon"></i>
+            Adelante
+          </button>
+        </div>
+      );
+    }
+  }
+}
 export default Paginator;
